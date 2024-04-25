b52f5c8492544d15e27052ba1c7caf457245def4
Alejandro Garcia <ale.iphone@gmail.com>
2024-04-25 22:08:40 +0200

    init

--- analyze_workouts.py
+++ analyze_workouts.py
@@ -5,40 +5,86 @@ with open("workouts.json", "r") as f:
     workouts = json.load(f)
 
 def filter_workouts():
-    # get 2023 workouts
+    # get 2024 workouts
     filtered_workouts = []
     for workout in workouts:
         date = workout["created_at"]
         date_obj = datetime.fromisoformat(date)
         # filter out unknown, misc, and walking
-        if workout["sport_id"] in (-1, 63, 71):
+        if workout["sport_id"] in (-1, 63, 71, 1):
             continue
         # filter out unscored, but keep unscorable
         if workout["score_state"] not in ("SCORED", "UNSCORABLE"):
             continue
 
-        if date_obj.year == 2023:
+        if date_obj.year == 2024:
             filtered_workouts.append(workout)
     return filtered_workouts
 
+def filter_padel_workouts():
+    # get padel workouts with sport ID 249
+    filtered_padel_workouts = []
+    for workout in workouts:
+        date = workout["created_at"]
+        date_obj = datetime.fromisoformat(date)
+        # filter to include only padel workouts
+        if workout["sport_id"] == 48:
+            filtered_padel_workouts.append(workout)
+    return filtered_padel_workouts
 
-wos_2023 = filter_workouts()
 
-num_workouts = len(wos_2023)
-print("Number of workouts in 2023:", num_workouts)
+
+wos_2024 = filter_workouts()
+
+def score_padel_workouts(workouts):
+    if not workouts:
+        return "No workouts to compare."
+
+    # Extract all average heart rates
+    heart_rates = [workout['score']['average_heart_rate'] for workout in workouts]
+
+    # Get the min and max heart rate values
+    min_hr = min(heart_rates)
+    max_hr = max(heart_rates)
+
+    # If all heart rates are the same, scoring isn't needed
+    if min_hr == max_hr:
+        return {workout["created_at"]: "100%" for workout in workouts}
+
+    # Calculate scores based on heart rates
+    scores = {}
+    for workout in workouts:
+
+        avg_hr = workout['score']['average_heart_rate']
+        print(avg_hr)
+        # Calculate score as a percentage
+        score = (avg_hr - min_hr) / (max_hr - min_hr) * 100
+        print(score)
+        date = workout["created_at"][:10]  # Extract only the date part
+        scores[date] = f"{score:.0f}%"  # Formatting score as a percentage
+
+    return scores
+
+# scores = score_padel_workouts(wos_2024)
+# for date, score in scores.items():
+#     print(f"{date}: {score}")
+
+
+num_workouts = len(wos_2024)
+print("Number of workouts in 2024:", num_workouts)
 
 # Get average kcal and fill missing values
-scored_workouts = [wo for wo in wos_2023 if wo["score_state"] == "SCORED"]
+scored_workouts = [wo for wo in wos_2024 if wo["score_state"] == "SCORED"]
 
 sum_kilojoule = sum(wo["score"]["kilojoule"] for wo in scored_workouts)
 average_kilojoule = sum_kilojoule / len(scored_workouts)
 
 # fill missing workouts with average kilojoule
-for wo in wos_2023:
+for wo in wos_2024:
     if wo["score_state"] == "UNSCORABLE":
         wo["score"] = {"kilojoule": average_kilojoule}
 
-sum_kilojoule = sum(x["score"]["kilojoule"] for x in wos_2023)
+sum_kilojoule = sum(x["score"]["kilojoule"] for x in wos_2024)
 sum_kcal = sum_kilojoule * 0.238846
 print(f"Total kcal: {sum_kcal:.2f} kcal")
 
@@ -48,7 +94,7 @@ print(f"Average kcal: {average_kcal:.2f} kcal")
 # Get total and average workout duration
 sum_duration = timedelta()
 
-for w in wos_2023:
+for w in wos_2024:
     start = datetime.fromisoformat(w["start"])
     end = datetime.fromisoformat(w["end"])
     duration = end - start
@@ -73,21 +119,112 @@ print(f"Average duration: {hh}:{mm}:{ss}")
 from collections import Counter, defaultdict
 
 SPORT_ID_TO_NAME = {
+    -1: "Activity",
     0: "Running",
-    1: "Bicycle",
+    1: "Cycling",
+    16: "Baseball",
+    17: "Basketball",
+    18: "Rowing",
+    19: "Fencing",
+    20: "Field Hockey",
+    21: "Football",
+    22: "Golf",
+    24: "Ice Hockey",
+    25: "Lacrosse",
+    27: "Rugby",
+    28: "Sailing",
+    29: "Skiing",
+    30: "Soccer",
+    31: "Softball",
+    32: "Squash",
     33: "Swimming",
-    96: "HIIT",
-    71: "MISC",
+    34: "Tennis",
+    35: "Track & Field",
+    36: "Volleyball",
+    37: "Water Polo",
+    38: "Wrestling",
+    39: "Boxing",
+    42: "Dance",
     43: "Pilates",
     44: "Yoga",
+    45: "Weightlifting",
+    47: "Cross Country Skiing",
     48: "Functional Fitness",
-    52: "Hiking",
-    -1: "Unknown Activity",
+    49: "Duathlon",
+    51: "Gymnastics",
+    52: "Hiking/Rucking",
+    53: "Horseback Riding",
+    55: "Kayaking",
+    56: "Martial Arts",
+    57: "Mountain Biking",
+    59: "Powerlifting",
+    60: "Rock Climbing",
+    61: "Paddleboarding",
+    62: "Triathlon",
     63: "Walking",
+    64: "Surfing",
+    65: "Elliptical",
+    66: "Stairmaster",
+    70: "Meditation",
+    71: "Other",
+    73: "Diving",
+    74: "Operations - Tactical",
+    75: "Operations - Medical",
+    76: "Operations - Flying",
+    77: "Operations - Water",
+    82: "Ultimate",
+    83: "Climber",
+    84: "Jumping Rope",
+    85: "Australian Football",
+    86: "Skateboarding",
+    87: "Coaching",
+    88: "Ice Bath",
+    89: "Commuting",
+    90: "Gaming",
+    91: "Snowboarding",
+    92: "Motocross",
+    93: "Caddying",
+    94: "Obstacle Course Racing",
+    95: "Motor Racing",
+    96: "HIIT",
+    97: "Spin",
+    98: "Jiu Jitsu",
+    99: "Manual Labor",
+    100: "Cricket",
+    101: "Pickleball",
+    102: "Inline Skating",
+    103: "Box Fitness",
+    104: "Spikeball",
+    105: "Wheelchair Pushing",
+    106: "Paddle Tennis",
+    107: "Barre",
+    108: "Stage Performance",
+    109: "High Stress Work",
+    110: "Parkour",
+    111: "Gaelic Football",
+    112: "Hurling/Camogie",
+    113: "Circus Arts",
+    121: "Massage Therapy",
+    125: "Watching Sports",
+    126: "Assault Bike",
+    127: "Kickboxing",
+    128: "Stretching",
+    230: "Table Tennis",
+    231: "Badminton",
+    232: "Netball",
+    233: "Sauna",
+    234: "Disc Golf",
+    235: "Yard Work",
+    236: "Air Compression",
+    237: "Percussive Massage",
+    238: "Paintball",
+    239: "Ice Skating",
+    240: "Handball",
+    249: "Padel"
 }
 
 workout_id_counters = defaultdict(int)
-for wo in wos_2023:
+for wo in wos_2024:
     workout_id_counters[SPORT_ID_TO_NAME[wo["sport_id"]]] += 1
 
 counter = Counter(workout_id_counters)
--- get_workouts.py
+++ get_workouts.py
@@ -1,16 +1,26 @@
 import json
 from whoop import WhoopClient
 
-username = "YOUR_USERNAME"
-password = "YOUR_PASSWORD"
+username = "ale.iphone@gmail.com"
+password = ""
 
-start_date="2022-10-12 23:59:59.999999"
+start_date="2024-02-26 23:59:59.999999"
 
 print("Get workouts...")
 with WhoopClient(username, password) as client:
     workouts = client.get_workout_collection(start_date=start_date)
+    recovery = client.get_recovery_collection(start_date=start_date)
+    sleep = client.get_sleep_collection(start_date=start_date)
 
 with open("workouts.json", "w") as f:
     json.dump(workouts, f, indent=4)
 
+with open("recovery.json", "w") as f:
+    json.dump(recovery, f, indent=4)
+
+with open("sleep.json", "w") as f:
+    json.dump(sleep, f, indent=4)
+
 print(f"Found {len(workouts)} workouts and saved it to `workouts.json`")
+print(f"Found {len(recovery)} recovery data and saved it to `recovery.json`")
+print(f"Found {len(sleep)} sleep data and saved it to `sleep.json`")
